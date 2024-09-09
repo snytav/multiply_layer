6e64b5e2efc2fe54794ab08202247f34ffdac0f5
Alexey Snytnikov <snytav@gmail.com>
2024-09-09 19:06:34 +0200

    optimization loop

--- multiply..py
+++ multiply..py
@@ -19,4 +19,23 @@ if __name__ == '__main__':
    mul = Multiply(3)
    x = torch.ones(3)
    y = mul(x)
+
+   criterion = nn.MSELoss(reduction='sum')
+   optimizer = torch.optim.SGD(mul.parameters(), lr=0.01)
+   y = torch.sin(x)
+
+   for t in range(200):
+       # Forward pass: Compute predicted y by passing x to the model
+       y_pred = mul(x)
+
+       # Compute and print loss
+       loss = criterion(y_pred, y)
+       print(t, loss.item())
+
+       # Zero gradients, perform a backward pass, and update the weights.
+       optimizer.zero_grad()
+       loss.backward()
+       optimizer.step()
+
+
    qq = 0
